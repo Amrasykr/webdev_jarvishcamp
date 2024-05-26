25910df2cf529d7ef572d0466d4f3ddfe0b23533
Ammar Asysyakur <114901699+Amrasykr@users.noreply.github.com>
2024-05-26 11:55:48 +0700

    lecture_8
    layouting sb admin

--- task_manager/resources/views/task/index.blade.php
+++ task_manager/resources/views/task/index.blade.php
@@ -1,49 +1,40 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
+@extends('layout.app')
 
-    <div class="container mt-5">
-        <h1 class="text-center">
-            Tasks List
-        </h1>
+@section('title', 'Tasks List')
 
-        <div class="d-flex flex-wrap justify-content-center gap-2 mt-4">
-            @foreach ($tasks as $task)
-            <div class="card w-25">
-                <div class="card-header d-flex justify-content-between">
-                    <div class="d-flex flex-row">
-                        <h6>{{$task['title']}}</h6>
-                    </div>
-                </div>
+@section('content')
+<div class="container-fluid px-4 mt-5">
+    <h1 class="text-center">
+        Tasks List
+    </h1>
 
-                <div class="card-body">
-                  <h5 class="card-title">{{$task['task']}}</h5>
-                  <p class="card-text">{{$task['description']}}</p>
-                <p>
-                    @if ($task['status'] == 'completed')
-                    <span class="badge rounded-pill bg-success">{{ $task['status'] }}</span>
-                @else
-                    <span class="badge rounded-pill bg-danger">{{ $task['status'] }}</span>
-                @endif
-                </p>
-                <p>
-                    {{ \Carbon\Carbon::parse($task['due_date'])->diffForHumans() }}
-
-                </p>
-                <a href="{{ url('/tasks/' . $task['id']) }}" class="btn btn-primary">Detail</a>
+    <div class="d-flex flex-wrap justify-content-center gap-2 mt-4">
+        @foreach ($tasks as $task)
+        <div class="card w-25">
+            <div class="card-header d-flex justify-content-between">
+                <div class="d-flex flex-row">
+                    <h6>{{$task['title']}}</h6>
                 </div>
             </div>
-            @endforeach
+
+            <div class="card-body">
+              <h5 class="card-title">{{$task['task']}}</h5>
+              <p class="card-text">{{$task['description']}}</p>
+            <p>
+                @if ($task['status'] == 'completed')
+                <span class="badge rounded-pill bg-success">{{ $task['status'] }}</span>
+            @else
+                <span class="badge rounded-pill bg-danger">{{ $task['status'] }}</span>
+            @endif
+            </p>
+            <p>
+                {{ \Carbon\Carbon::parse($task['due_date'])->diffForHumans() }}
+
+            </p>
+            <a href="{{ url('/tasks/' . $task['id']) }}" class="btn btn-primary">Detail</a>
+            </div>
         </div>
+        @endforeach
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+</div>
+@endsection
